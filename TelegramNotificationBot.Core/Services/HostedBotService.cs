639b87714cdc7ed3c2b1e2e746b378392f2804bd
Gaein_nidb <mail@gaein.cn>
2024-11-04 19:26:03 +0800

    fix: remove datetime

--- TelegramNotificationBot.Core/Services/HostedBotService.cs
+++ TelegramNotificationBot.Core/Services/HostedBotService.cs
@@ -23,7 +23,7 @@ internal class HostedBotService(
         var webhookAddress = options.Value.BotWebhookUrl.ToString();
         logger.LogInformation("Setting webhook: {WebhookAddress}", webhookAddress);
         await botClient.SetWebhook(
-           url: webhookAddress + "#" + DateTimeOffset.Now.ToString("s"),
+           url: webhookAddress,
            allowedUpdates: Array.Empty<UpdateType>(),
            secretToken: options.Value.SecretToken,
            cancellationToken: cancellationToken);
